8228c92170d0684cf17b4a8c8c73f7fab8126833
Gunnar Raßmann <git@rassmann.dev>
2026-09-10 20:18:32 +0200

    improve rcl installation script

--- build_files/install_rcl.sh
+++ build_files/install_rcl.sh
@@ -4,12 +4,7 @@ set -ouex pipefail
 
 # fetch the latest release build download url
 RCL_URL=$(curl -s "https://api.github.com/repos/ruuda/rcl/releases" |
-            jq -r '
-              sort_by(.created_at) | reverse | .[0]   # newest release
-              | .assets[]
-              | select(.name | endswith("x86_64-unknown-linux-gnu"))
-              | .browser_download_url
-            ')
+            jq -r '[ .[] | .assets[] | select(.name | endswith("x86_64-unknown-linux-gnu")) ][0].browser_download_url')
 
 
 echo "Downloading/installing rcl"
